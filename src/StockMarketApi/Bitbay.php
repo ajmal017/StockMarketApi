5c51fa2cf3b520861a9250cf1cbe9ddd5699cf20
bobo1212 <bobo1212@wp.pl>
2017-10-08 10:03:35 +0200

    BitBay BitMarket

--- src/StockMarketApi/Bitbay.php
+++ src/StockMarketApi/Bitbay.php
@@ -2,14 +2,12 @@
 
 namespace StockMarketApi;
 
-use StockMarketApi\Api;
 
-class BitMarket{
+class Bitbay {
 
     public function orderbook() {
-        $orderbookJson = file_get_contents('https://www.bitmarket.pl/json/BTCPLN/orderbook.json');
+        $orderbookJson = file_get_contents('https://bitbay.net/API/Public/BTCPLN/orderbook.json');
         $orderbook = json_decode($orderbookJson);
         return $orderbook;
     }
-
 }
